7efeaca638d596b0d8a1c7c60092b3a8c8a3995a
salmashifa <salmashifaaa00@gmail.com>
2026-06-06 10:34:06 +0700

    halaman about

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('Halaman About');
-});
+    return view('welcome');
+}); 
+
+use App\Http\Controllers\AboutController;
+Route::get('/about', [AboutController::class, 'index'])->name('about');
